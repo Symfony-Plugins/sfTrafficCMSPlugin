667f34377555d7cda2bda24539ec003a92389c9e
uncleringo <uncleringo@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-03-15 08:06:13 -0500

    Added hasAccess function
    git-svn-id: http://svn.symfony-project.com/plugins/sfTrafficCMSPlugin/trunk@28530 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfTrafficGuardSecurityUser.class.php
+++ lib/sfTrafficGuardSecurityUser.class.php
@@ -16,4 +16,49 @@ class sfTrafficGuardSecurityUser extends sfGuardSecurityUser
 
     return false;
   }
+  
+  public function hasAccess()
+  {
+    $allowed = false;
+
+    /**
+     * Check for specified allowed groups
+     */
+    if ($groups = sfConfig::get('app_sf_guard_plugin_allowed_groups', false))
+    {
+      foreach ($this->getGroups() as $group)
+      {
+        if (in_array($group->name, $groups))
+        {
+          $allowed = true;
+          break;
+        }
+      }
+    }
+    /**
+     * Check for specified denied groups
+     */
+    else if ($groups = sfConfig::get('app_sf_guard_plugin_denied_groups', false))
+    {
+      $allowed = true;
+
+      foreach ($this->getGroups() as $group)
+      {
+        if (in_array($group->name, $groups))
+        {
+          $allowed = false;
+          break;
+        }
+      }
+    }
+    /**
+     * Nothing specified means we're ok
+     */
+    else
+    {
+      $allowed = true;
+    }
+
+    return $allowed;
+  }
 }
