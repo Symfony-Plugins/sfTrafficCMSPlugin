74b889e8a6242704c3f472fa0e285fdba5338b5a
uncleringo <uncleringo@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-03-26 10:32:11 -0500

    Added some css for the navigation menu
    git-svn-id: http://svn.symfony-project.com/plugins/sfTrafficCMSPlugin/trunk@28804 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfTrafficCMSPlugin/templates/_navigation_menu.php
+++ modules/sfTrafficCMSPlugin/templates/_navigation_menu.php
@@ -1,17 +1,17 @@
 <?php if ($sf_user->isAuthenticated()): ?>
-  <div id="menu">
+  <div id="cms_menu">
     <ul>
     <?php $nav_items = sfConfig::get('app_sf_traffic_cms_plugin_navigation') ?>
     <?php $config = sfConfig::get('app_sf_traffic_cms_plugin_auto_configure') ?>
     <?php foreach ($nav_items as $model => $options): ?>
       <?php $singleton = (isset($config['models'][$model]['singleton']) && $config['models'][$model]['singleton']) ? $config['models'][$model]['singleton'] : false ?>
       <?php if(!isset($options['credential']) || $sf_user->hasCredential($options['credential'])): ?>
-      <li style="float: left; list-style-type: none; margin-right: 10px">
+      <li>
         <?php echo link_to($options['label'], '@' . $model . ($singleton ? '_edit?id=1' : '')) ?>
       </li>
       <?php endif ?>
     <?php endforeach ?>
+      <li><?php echo link_to('Log out', '@sf_guard_signout') ?></li>
     </ul>
   </div>
-  <br />
 <?php endif; ?>
