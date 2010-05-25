ded2eed3f447a66dfd826d8e89ec7c670fea138f
uncleringo <uncleringo@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-05-25 08:25:34 -0500

    Added display of any images to simple pages
    git-svn-id: http://svn.symfony-project.com/plugins/sfTrafficCMSPlugin/trunk@29619 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfTrafficCMSPlugin/templates/showSubPageSuccess.php
+++ modules/sfTrafficCMSPlugin/templates/showSubPageSuccess.php
@@ -1,3 +1,6 @@
 <h1><?php echo $subPage->title ?></h1>
 <?php include_partial('sfTrafficCMSPlugin/subPageMenu', array('page' => $subPage->Parent, 'currentPage' => $subPage)) ?>
+<?php foreach ($subPage->getImages() as $image): ?>
+<img src="<?php echo $image->getImageSrc() ?>" />
+<?php endforeach ?>
 <div id="body"><?php echo $sf_data->getRaw('subPage')->body_copy ?></div>
--- modules/sfTrafficCMSPlugin/templates/simplePageSuccess.php
+++ modules/sfTrafficCMSPlugin/templates/simplePageSuccess.php
@@ -1,3 +1,6 @@
 <h1><?php echo $page->title ?></h1>
 <?php include_partial('sfTrafficCMSPlugin/subPageMenu', array('page' => $page, 'currentPage' => $page)) ?>
+<?php foreach ($page->getImages() as $image): ?>
+<img src="<?php echo $image->getImageSrc() ?>" />
+<?php endforeach ?>
 <div id="body"><?php echo $sf_data->getRaw('page')->body_copy ?></div>
