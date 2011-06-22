cd8ca3d6913da1d3b91211424609fa7c6eaa6c3c
uncleringo <uncleringo@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-06-22 11:20:20 -0500

    fix for sub nav credentials
    git-svn-id: http://svn.symfony-project.com/plugins/sfTrafficCMSPlugin/trunk@32664 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfTrafficCMSPlugin/templates/_sub_nav.php
+++ modules/sfTrafficCMSPlugin/templates/_sub_nav.php
@@ -1,26 +1,26 @@
 <?php if ($sf_user->isAuthenticated()): ?>
   <?php $routes = sfContext::getInstance()->getRouting()->getRoutes(); ?>
-  
 
-  <?php $config = sfConfig::get('app_sf_traffic_cms_plugin_auto_configure') ?>
+  <?php $config = sfConfig::get('app_sf_traffic_cms_plugin_auto_configure'); ?>
   
     <ul>
     <?php foreach ($sub_nav_items as $model => $options): ?>
       <?php $singleton = (isset($config['models'][$model]['singleton']) && $config['models'][$model]['singleton']) ? $config['models'][$model]['singleton'] : false ?>
-      <?php if(!isset($options['credential']) || $sf_user->hasCredential($options['credential'])): ?>
-      <?php if(isset($routes[$model])): ?>
-      <li>
-        <?php echo link_to($options['label'], '@' . $model . ($singleton ? '_edit?id=1' : '')); ?>
-      <?php else: ?>
-      <li>
-        <a class="no-href"><?php echo $options['label'];?></a>
-      <?php endif; ?>
-        <?php
-        if(isset($options['sub_menu'])):
-          include_partial('sub_nav', array('options' => $options['sub_menu']));
-        endif;
-        ?>
-      </li>
+
+      <?php if(!isset($options['credential']) || $sf_user->hasCredential($options['credential']->getRawValue()) ): ?>
+        <?php if(isset($routes[$model])): ?>
+        <li>
+          <?php echo link_to($options['label'], '@' . $model . ($singleton ? '_edit?id=1' : '')); ?>
+        <?php else: ?>
+        <li>
+          <a class="no-href"><?php echo $options['label'];?></a>
+        <?php endif; ?>
+          <?php
+          if(isset($options['sub_menu'])):
+            include_partial('sub_nav', array('options' => $options['sub_menu']));
+          endif;
+          ?>
+        </li>
       <?php endif ?>
     <?php endforeach ?>
     </ul>
